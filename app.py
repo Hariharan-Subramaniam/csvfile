e6f1d510eb62aa39bf7049d42b9f82d6f97fd48f
Hariharan-Subramaniam <81857147+Hariharan-Subramaniam@users.noreply.github.com>
2021-06-19 08:29:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__) 
 # mongoDB configuration
-app.config["MONGO_URI"]="mongodb+srv://hari:12345@cluster0.avyit.mongodb.net/csvfile?retryWrites=true&w=majority"
+app.config["MONGO_URI"]="mongodb+srv://****:*******@cluster0.******.mongodb.net/csvfile?retryWrites=true&w=majority"
 mongo =PyMongo(app) 
 db=mongo.db
 logindetails=db.logindetails
